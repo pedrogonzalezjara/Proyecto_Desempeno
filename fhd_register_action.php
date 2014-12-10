7e3389d4857058cc60c2b360ffee9602387c4347
Pedro <pdro.gonzalez@gmail.com>
2014-12-10 15:49:15 -0300

    fix

--- fhd_register_action.php
+++ fhd_register_action.php
@@ -13,13 +13,13 @@ include("includes/functions.php");
 //initilize db
 $db = new ezSQL_mysqli(db_user,db_password,db_name,db_host);
 
-<<<<<<< HEAD
+
 if (ALLOW_REGISTER <> "yes"){
 	echo "<p>El registro esta cerrado</p>";
-=======
+
 if (ALLOW_REGISTER <> "no"){
 	echo "<p>Registration is Closed</p>";
->>>>>>> origin/master
+
 	include("includes/footer.php");
 	exit;
 	}
